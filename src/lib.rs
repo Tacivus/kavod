df72883a53c2a944d2d9639ec7b6a1aa2f3a2484
Tacivus <github@tacivus.com>
2026-07-11 22:30:27 -0400

    Gutted old code

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,7 @@
 #![allow(dead_code)]
 
-pub mod cache;
 pub mod clock;
-pub mod context;
 pub mod decimal;
-pub mod graph;
-pub mod handler;
-pub mod kernel;
-pub mod log;
-pub mod message;
 pub mod price;
 pub mod quantity;
-pub mod reducer;
-pub mod schedule;
 pub mod time;
--- src/message.rs
+++
@@ -1,4 +0,0 @@
-use std::{any::Any, fmt::Debug};
-
-/// Message is the core trait that makes the entire system work
-pub trait Message: Send + Debug + Any + 'static {}
